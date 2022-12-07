ff6fc204b5f3152ed9d54dadb0a2ebff53984583
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2022-12-06 22:26:48 -0300

    EXERCICIO009
    EXERCICIO009

---
+++ LACO_DE_REPETICAO/app.js
@@ -0,0 +1,11 @@
+function Tabuada(){
+ let contador = 0
+ let n = document.getElementById("numero").value   
+    
+document.getElementById("resposta").innerHTML = ""
+while (contador <=10) {
+     
+
+document.getElementById("resposta").innerHTML += n + "X" + contador + "=" + contador * n + "<br>"
+contador = contador + 1}
+}
